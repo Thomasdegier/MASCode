6d34f7fcf9696367bf0861c7d68e3e8959d14968
Jonathan Mitnik <JMitnik@gmail.com>
2018-10-16 20:50:09 +0200

    Add policy 3d matrix

--- Gridworld/Gridworld.py
+++ Gridworld/Gridworld.py
@@ -6,13 +6,14 @@ LOCATION_A_PRIME = (1, 4)
 LOCATION_B = (3, 0)
 LOCATION_B_PRIME = (3, 2)
 
-POLICY_P = 0.25
+INIT_POLICY_P = 0.25
 GAMMA = 0.9
 
 TRANS_A_REWARD = 10
 TRANS_B_REWARD = 5
 OUT_OF_BOUNDS_REWARD = -1
 STANDARD_REWARD = 0
+NR_ACTIONS = 4
 
 DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 UP, RIGHT, DOWN, LEFT = DIRECTIONS
@@ -21,6 +22,7 @@ UP, RIGHT, DOWN, LEFT = DIRECTIONS
 class GridWorld:
     def __init__(self, dim_x=5, dim_y=5):
         self.grid = np.zeros((dim_x, dim_y))
+        self.policies = np.full((dim_x, dim_y, NR_ACTIONS), INIT_POLICY_P)
         self.max_x = dim_x - 1
         self.max_y = dim_y - 1
 
@@ -36,13 +38,18 @@ class GridWorld:
     def getValueForState(self, state_tuple):
         initial_value = 0
 
-        for action in DIRECTIONS:
+        for index, action in enumerate(DIRECTIONS):
             next_state_tuple = self.getNextState(state_tuple, action)
             expected_reward = self.getExpectedReward(
                 state_tuple, next_state_tuple)
-            initial_value += POLICY_P * (expected_reward + (GAMMA * self.getValueForNextState(state_tuple, next_state_tuple)))
+            initial_value += self.getPolicy(state_tuple, index) * (expected_reward + (
+                GAMMA * self.getValueForNextState(state_tuple, next_state_tuple)))
 
         self.grid.transpose()[state_tuple] = initial_value
+
+    def getPolicy(self, state_tuple, action_index):
+        x, y = state_tuple
+        return self.policies[y, x, action_index]
 
     def getValueForNextState(self, current_state_tuple, next_state_tuple):
         if self.isOutOfBounds(next_state_tuple):
@@ -78,6 +85,7 @@ class GridWorld:
                 self.getValueForState(coordinates)
 
     def run(self):
+
         for i in range(100):
             self.traverseGrid()
 
