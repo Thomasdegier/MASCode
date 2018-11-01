acfb4727ba3c61b8edf40a0d36d3e1c6628ea5d8
Jonathan Mitnik <JMitnik@gmail.com>
2018-11-01 17:00:41 +0100

    QLearning working on gridworld too

--- Cliff Walking/main.py
+++ Cliff Walking/main.py
@@ -6,16 +6,16 @@ from QLearnerAgent import QLearnerAgent
 # Cliff World
 ########################################################
 
-cliffWorld = CliffEnvironment(10, 4)
-QagentC = QLearnerAgent(cliffWorld, 10000, 0.2, (0, 3))
-QagentC.run()
+# cliffWorld = CliffEnvironment(10, 4)
+# QagentC = QLearnerAgent(cliffWorld, 10000, 0.2, (0, 3))
+# QagentC.run()
 ########################################################
 # Grid World
 ########################################################
 
-# gridWorld = GridEnvironment(8, 8)
-# QagentG = QLearnerAgent(gridWorld, 100, 0.2, (0, 0))
-# QagentG.run()
+gridWorld = GridEnvironment(8, 8)
+QagentG = QLearnerAgent(gridWorld, 10000, 0.2, (0, 0))
+QagentG.run()
 
 
 # TODO: Q-learning gaat nog niet lekker, kennelijk gebeurt er nog iets geks. 
